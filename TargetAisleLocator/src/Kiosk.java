d9a244937c05c02dd0ec40b9e2023ff126e061cd
Gwen <gcm3807@cs.rit.edu>
2018-05-18 17:53:13 -0400

    Added file reading functionality.

--- TargetAisleLocator/src/Kiosk.java
+++ TargetAisleLocator/src/Kiosk.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * ${CLASS_NAME}
  * Created by Gwen on 5/18/2018.
@@ -5,4 +12,21 @@
  * Revisions: ${LOG}
  */
 public class Kiosk {
+    public HashMap<Product,String> products;
+
+    public static void main(String[] args){
+        try{
+            File file = new File("products.txt");
+            FileReader fr = new FileReader (file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while(( line = br.readLine()) != null){
+                String[] readLine = line.split(",");
+            } //end while
+
+        }catch(Exception e){
+            System.out.println("The file does not exist. Exiting program...");
+            System.exit(0);
+        }
+    }
 }
