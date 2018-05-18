e907f104bf6a2ff7969065d097ad278781930242
Gwen <gcm3807@cs.rit.edu>
2018-05-18 18:30:33 -0400

    Added new class for file reading functionality

---
+++ TargetAisleLocator/src/FileLoader.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.HashMap;
+
+/**
+ * ${CLASS_NAME}
+ * Created by Gwen on 5/18/2018.
+ * Version number: ${VERSION}.
+ * Revisions: ${LOG}
+ */
+public class FileLoader {
+    private HashMap<Product, String> products;
+
+    public FileLoader() throws IOException{
+        File file = new File("products.txt");
+        FileReader fr = new FileReader (file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while(( line = br.readLine()) != null){
+            String[] readLine = line.split(",");
+            Product newProduct = new Product();
+            newProduct.setName(readLine[1]);
+            products.put(newProduct, readLine[2]);
+        } //end while
+    }
+
+    public HashMap<Product, String> getProducts(){return products;}
+}
